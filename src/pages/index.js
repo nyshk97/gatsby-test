a5d916db75cbc74e7addc0c1612c541f32fd40e5
tsubasa namatame <nyshk97@gmail.com>
2020-05-06 15:18:54 +0900

    変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <p>変更変更変更</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
